391d005bf28758e6cb0b4ade8b3cd0da9faa7ae0
HcGys <370379459@qq.com>
2024-09-08 00:47:52 +0800

    文章更新

--- js/plugins/voice.js
+++ js/plugins/voice.js
@@ -1,11 +1,6 @@
-function createVoiceDom(r){let c="";for(let a=0;a<r.length;++a){let e=r[a];e.currentTime;var o=Math.floor(e.duration/10)+2,n=o+2,i=e.parentElement,l=i.querySelector(".pause-btn"),d=i.querySelector(".play-btn"),s=(i.querySelector(".voice-seconds").innerHTML=Math.floor(o/10)+'"',l.addEventListener("click",()=>{e.pause()}),d.addEventListener("click",()=>{e.play()}),document.createElement("div"));s.className="voice-wave-"+a;for(let e=0;e<n;e++){var v=document.createElement("span");v.className="voice-wave-item",s.append(v)}i.insertBefore(s,i.querySelector(".voice-metas"));let t=`
-            .voice-wave-${a+1} {
-                width: 3px;
-                border-radius: 3px;
-            }
-        `;for(let e=0;e<n;++e)t+=`
-            .voice-wavei-${a+1} span.voice-wave-item:nth-child(${e+1}) {
+function createVoiceDom(o){let c="";for(let n=0;n<o.length;++n){let e=o[n];e.currentTime;var r=Math.floor(e.duration/10)+2,i=r+2,s=e.parentElement;let t=s.querySelector(".pause-btn"),a=s.querySelector(".play-btn");s.querySelector(".voice-seconds").innerHTML=Math.floor(r/10)+'"',t.addEventListener("click",()=>{e.pause(),t.style.display="none",a.style.display="block"}),a.addEventListener("click",()=>{e.play(),t.style.display="block",a.style.display="none"});var d=document.createElement("div");d.className="voice-wave-"+(n+1);for(let e=0;e<i;e++){var m=document.createElement("span");m.className="voice-wave-item",d.append(m)}s.insertBefore(d,s.querySelector(".voice-metas"));let l="";for(let e=0;e<i;++e)l+=`
+            .voice-wavei-${n+1} span.voice-wave-item:nth-child(${e+1}) {
                 height: ${10*(Math.floor(8*Math.random())+3)}%;
                 background: var(--chat-voice-block);
             }
-            `;c+=t}var e=document.createElement("style");e.setAttribute("type","text/css"),e.innerHTML=c,document.getElementsByTagName("head").item(0).appendChild(e)}
+            `;c+=l}var e=document.createElement("style");e.setAttribute("type","text/css"),e.innerHTML=c,document.getElementsByTagName("head").item(0).appendChild(e)}
