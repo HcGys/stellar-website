9c58a4117d4c25dac7c4451320c4959affa9f207
HcGys <370379459@qq.com>
2024-09-08 00:28:18 +0800

    文章更新

--- js/plugins/voice.js
+++ js/plugins/voice.js
@@ -1,11 +1,11 @@
-function createVoiceDom(c){let n="";for(let a=0;a<c.length;++a){let e=c[a];e.currentTime;var r=e.duration,i=r+2,o=e.parentElement,l=o.querySelector(".pause-btn"),d=o.querySelector(".play-btn"),v=(o.querySelector(".voice-seconds").innerHTML=""+r,l.addEventListener("click",()=>{e.pause()}),d.addEventListener("click",()=>{e.play()}),document.createElement("div"));v.className="voice-wave-"+a;for(let e=0;e<i;e++){var m=document.createElement("span");m.className="voice-wave-item",v.append(m)}let t=`
+function createVoiceDom(r){let c="";for(let a=0;a<r.length;++a){let e=r[a];e.currentTime;var n=e.duration,o=n+2,i=e.parentElement,l=i.querySelector(".pause-btn"),d=i.querySelector(".play-btn"),s=(i.querySelector(".voice-seconds").innerHTML=floor(n/10)+'"',l.addEventListener("click",()=>{e.pause()}),d.addEventListener("click",()=>{e.play()}),document.createElement("div"));s.className="voice-wave-"+a;for(let e=0;e<o;e++){var v=document.createElement("span");v.className="voice-wave-item",s.append(v)}s.insertBefore(i.querySelector(".voice-metas"));let t=`
             .voice-wave-${a} {
                 width: 3px;
                 border-radius: 3px;
             }
-        `;for(let e=0;e<i;++e)t+=`
+        `;for(let e=0;e<o;++e)t+=`
             .voice-wavei-${a} span.voice-wave-item:nth-child(${e}) {
                 height: ${10*(Math.floor(8*Math.random())+3)}%;
                 background: var(--chat-voice-block);
             }
-            `;n+=t}var e=document.createElement("style");e.setAttribute("type","text/css"),e.innerHTML=n,document.getElementsByTagName("head").item(0).appendChild(e)}
+            `;c+=t}var e=document.createElement("style");e.setAttribute("type","text/css"),e.innerHTML=c,document.getElementsByTagName("head").item(0).appendChild(e)}
