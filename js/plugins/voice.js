2882b505eaad4f99f81863352936fda25c6907c0
HcGys <370379459@qq.com>
2024-09-08 22:48:41 +0800

    文章更新

--- js/plugins/voice.js
+++ js/plugins/voice.js
@@ -1,16 +1,16 @@
-function createVoiceDom(r){for(let l=0;l<r.length;++l){let e=r[l];e.loop=!1;e.currentTime;let t=e.parentElement,a=t.querySelector(".pause-btn"),s=t.querySelector(".play-btn"),n=t.querySelector(".voice-seconds"),i;isNaN(e.duration)||e.duration===1/0?e.addEventListener("durationchange",()=>{i=createCssStyle(e,t,n,l)}):i=createCssStyle(e,t,n,l),e.addEventListener("ended",()=>{e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,i.classList.remove("active"),i.classList.add("back")}),t.addEventListener("click",()=>{"flex"===a.style.display?(e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,i.classList.remove("active"),i.classList.add("back")):(e.play(),a.style.display="flex",s.style.display="none",i.classList.add("active"),i.classList.remove("back"))})}}function createCssStyle(e,t,a,s){var e=e.duration,n=Math.floor(e),i=25<n+2?25:n+2,a=(a.innerHTML=`${99<n?"..":n}"`,2*(2*i-1)),l=(25<n+2&&(t.style.width="100%"),document.createElement("div")),n=document.createElement("div");n.className="play-line",l.append(n),l.className="voice-wave voice-wave-"+(s+1);for(let e=0;e<i;e++){var r=document.createElement("span");r.className="voice-wave-item",l.append(r)}t.insertBefore(l,t.querySelector(".voice-metas"));let o=`
-    .voice-wave.voice-wave-${s+1}>.play-line {
+function createVoiceDom(o){for(let r=0;r<o.length;++r){let e=o[r];e.loop=!1;e.currentTime;let t=e.parentElement,a=t.querySelector(".pause-btn"),n=t.querySelector(".play-btn"),s=t.querySelector(".voice-seconds"),i,l=(isNaN(e.duration)||e.duration===1/0?e.addEventListener("durationchange",()=>{i=createCssStyle(e,t,s,r)}):i=createCssStyle(e,t,s,r),i.querySelector("play-line"));e.addEventListener("ended",()=>{e.pause(),a.style.display="none",n.style.display="flex",e.currentTime=0,l.classList.remove("active"),l.classList.add("back")}),t.addEventListener("click",()=>{"flex"===a.style.display?(e.pause(),a.style.display="none",n.style.display="flex",e.currentTime=0,l.classList.remove("active"),l.classList.add("back")):(e.play(),a.style.display="flex",n.style.display="none",l.classList.add("active"),l.classList.remove("back"))})}}function createCssStyle(e,t,a,n){var e=e.duration,s=Math.floor(e),i=25<s+2?25:s+2,a=(a.innerHTML=`${99<s?"..":s}"`,2*(2*i-1)),l=(25<s+2&&(t.style.width="100%"),document.createElement("div")),s=document.createElement("div");s.className="play-line",l.append(s),l.className="voice-wave voice-wave-"+(n+1);for(let e=0;e<i;e++){var r=document.createElement("span");r.className="voice-wave-item",l.append(r)}t.insertBefore(l,t.querySelector(".voice-metas"));let o=`
+    .voice-wave.voice-wave-${n+1}>.play-line {
         transition: transform ${e}s linear;
         -moz-transition: transform ${e}s linear;
         -webkit-transition: transform ${e}s linear;
         -o-transition: transform ${e}s linear;
     }
-    .voice-wave.voice-wave-${s+1}>.play-line.active {
+    .voice-wave.voice-wave-${n+1}>.play-line.active {
         transform: translateX(${a}px);
     }
     `;for(let e=0;e<i;++e)o+=`
-        .voice-wave-${s+1} span.voice-wave-item:nth-child(${e+1}) {
+        .voice-wave-${n+1} span.voice-wave-item:nth-child(${e+1}) {
             height: ${10*(Math.floor(8*Math.random())+3)}%;
             margin-left: ${0==e?0:2}px;
         }
-        `;n=document.createElement("style");return n.setAttribute("type","text/css"),n.innerHTML=o,document.getElementsByTagName("head").item(0).appendChild(n),l}
+        `;s=document.createElement("style");return s.setAttribute("type","text/css"),s.innerHTML=o,document.getElementsByTagName("head").item(0).appendChild(s),l}
