ccac085a3e8ab74bff2958a147524abf7e51951f
HcGys <370379459@qq.com>
2024-09-08 22:03:21 +0800

    文章更新

--- js/plugins/voice.js
+++ js/plugins/voice.js
@@ -1,13 +1,13 @@
-function createVoiceDom(r){for(let n=0;n<r.length;++n){let e=r[n];e.loop=!1;e.currentTime;let t=e.parentElement,a=t.querySelector(".pause-btn"),s=t.querySelector(".play-btn"),i=t.querySelector(".voice-seconds"),l;isNaN(e.duration)||e.duration===1/0?e.addEventListener("durationchange",()=>{l=createCssStyle(e,t,i,n)}):l=createCssStyle(e,t,i,n),e.addEventListener("ended",()=>{e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,l.classList.remove("active"),l.classList.add("back")}),t.addEventListener("click",()=>{"flex"===a.style.display?(e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,l.classList.remove("active"),l.classList.add("back")):(e.play(),a.style.display="flex",s.style.display="none",l.classList.add("active"),l.classList.remove("back"))})}}function createCssStyle(e,t,a,s){var e=e.duration,i=Math.floor(e),l=25<i+2?25:i+2,n=(a.innerHTML=`${99<i?"..":i}"`,25<i+2&&(t.style.width="100%"),document.createElement("div"));n.className="voice-wave voice-wave-"+(s+1);for(let e=0;e<l;e++){var r=document.createElement("span");r.className="voice-wave-item",n.append(r)}t.insertBefore(n,t.querySelector(".voice-metas"));let c=`
+function createVoiceDom(l){for(let r=0;r<l.length;++r){let e=l[r];e.loop=!1;e.currentTime;let t=e.parentElement,a=t.querySelector(".pause-btn"),s=t.querySelector(".play-btn"),n=t.querySelector(".voice-seconds"),i;isNaN(e.duration)||e.duration===1/0?e.addEventListener("durationchange",()=>{i=createCssStyle(e,t,n,r)}):i=createCssStyle(e,t,n,r),e.addEventListener("ended",()=>{e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,i.classList.remove("active"),i.classList.add("back")}),t.addEventListener("click",()=>{"flex"===a.style.display?(e.pause(),a.style.display="none",s.style.display="flex",e.currentTime=0,i.classList.remove("active"),i.classList.add("back")):(e.play(),a.style.display="flex",s.style.display="none",i.classList.add("active"),i.classList.remove("back"))})}}function createCssStyle(e,t,a,s){var e=e.duration,n=Math.floor(e),i=25<n+2?25:n+2,r=(a.innerHTML=`${99<n?"..":n}"`,25<n+2&&(t.style.width="100%"),document.createElement("div"));r.className="voice-wave voice-wave-"+(s+1);for(let e=0;e<i;e++){var l=document.createElement("span");l.className="voice-wave-item",r.append(l)}t.insertBefore(r,t.querySelector(".voice-metas"));let o=`
     .voice-wave.voice-wave-${s+1}::before {
-        transition: left ${e}s linear;
-        -moz-transition: left ${e}s linear;
-        -webkit-transition: left ${e}s linear;
-        -o-transition: left ${e}s linear;
+        transition: transform ${e}s linear;
+        -moz-transition: transform ${e}s linear;
+        -webkit-transition: transform ${e}s linear;
+        -o-transition: transform ${e}s linear;
     }
-    `;for(let e=0;e<l;++e)c+=`
+    `;for(let e=0;e<i;++e)o+=`
         .voice-wave-${s+1} span.voice-wave-item:nth-child(${e+1}) {
             height: ${10*(Math.floor(8*Math.random())+3)}%;
             margin-left: ${0==e?0:2}px;
         }
-        `;a=document.createElement("style");return a.setAttribute("type","text/css"),a.innerHTML=c,document.getElementsByTagName("head").item(0).appendChild(a),n}
+        `;a=document.createElement("style");return a.setAttribute("type","text/css"),a.innerHTML=o,document.getElementsByTagName("head").item(0).appendChild(a),r}
